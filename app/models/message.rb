627b68c014fe902dc6a8b6fe0ad1dd19381b1283
cpintoval <cpintovalderrama@gmail.com>
2015-09-13 21:01:07 +0000

    Added URL field. Validation with regex not working

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,15 @@
 class Message < ActiveRecord::Base
 
   validates :content, :author, presence: true
+  validate :url_validator
+
+  private
+  def url_validator
+    if url
+      # unless =~ /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+      #   errors.add(:url, "invalid url format")
+      # end
+    end
+  end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913192103) do
+ActiveRecord::Schema.define(version: 20150913201631) do
 
   create_table "messages", force: :cascade do |t|
     t.string "author"
     t.string "content"
+    t.string "url"
   end
 
 end
